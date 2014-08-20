2eb815eaa87736679d341ea673505fbca049a02c
Dirk Sierd de Vries <dirksierd@me.com>
2014-08-20 19:58:58 +0200

    added better titles

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,6 +4,8 @@ class StaticPagesController < ApplicationController
   GUIDE_URL = 'https://raw.githubusercontent.com/NHLHogeschool/the-next-web/master/README.md'
 
   def guide
+    # TODO: Add caching
+    @title = 'Handboek'
     @body  = open(GUIDE_URL).read
     render :show
   end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,9 @@
 module ApplicationHelper
+  def title(title)
+    parts = ['The Next Web', title].delete_if(&:blank?)
+    content_for :title, parts.join(': ')
+  end
+
   def markdown(body)
     markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML,
                                        autolink: true,
