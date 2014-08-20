f0b4444061d33ec149dd3632414fce412ecc29d5
Dirk Sierd de Vries <dirksierd@me.com>
2014-08-20 23:01:13 +0200

    added some forms to add new posts + authentication

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,37 @@
 class PostsController < ApplicationController
+  before_action :authenticate, except: :index
+
   def index
     @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new post_params
+    if @post.save
+      redirect_to @post
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == Rails.application.secrets.cms_username &&
+      password == Rails.application.secrets.cms_password
+    end
+
+    session[:logged_in] = true
+
+    return true
   end
 end
