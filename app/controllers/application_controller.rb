803dcd1a4627da1c18dec657757d8c19e41ba7c1
Dirk Sierd de Vries <dirksierd@me.com>
2014-08-21 11:47:23 +0200

    empty agenda-note

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  helper_method :logged_in?
+  helper_method :logged_in?, :upcoming_events_by_date
 
   def logged_in?
     session.key?(:logged_in)
+  end
+
+  def upcoming_events_by_date
+    @upcoming_events_by_date ||= Event.upcoming_by_date
   end
 end
