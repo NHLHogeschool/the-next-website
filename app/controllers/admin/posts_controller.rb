697caf82046e9af96128c0f321d0b10edb892cf3
Dirk Sierd de Vries <dirksierd@me.com>
2014-08-25 12:22:25 +0200

    added cms-backend for posts

---
+++ app/controllers/admin/posts_controller.rb
@@ -0,0 +1,60 @@
+class Admin::PostsController < ApplicationController
+  before_action :authenticate
+
+  def index
+    @posts = Post.order('created_at desc').all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new post_params
+    if @post.save
+      redirect_to @post
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    redirect_to posts_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == Rails.application.secrets.cms_username &&
+      password == Rails.application.secrets.cms_password
+    end
+
+    session[:logged_in] = true
+
+    true
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
   resources :posts, path: 'berichten'
   get 'handboek' => 'static_pages#guide', as: :guide
 
+  get '/admin' => 'posts#index', as: :admin
+  namespace :admin do
+    resources :posts
+  end
+
   # Calendar
   get 'google_calendar' => redirect('https://www.google.com/calendar/embed?src=mo7vph6l2neh6m3bcju1ncon4k@group.calendar.google.com&ctz=Europe/Amsterdam'), as: :google_calendar
   get 'auth/google_oauth2/callback' => 'static_pages#callback'
